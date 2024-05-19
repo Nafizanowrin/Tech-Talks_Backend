a0795b335d23a31fb4de6fe3b3879de58585356d
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2024-05-19 14:47:04 +0600

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/news', newsRoutes);
 const PORT = 5000;
 connectDB().then(() => {
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
 
 });
